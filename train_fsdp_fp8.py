Fixed is_first_mircobatch calculation.

train_fsdp_fp8.py
@@ -24,6 +24,8 @@ from torch.distributed.fsdp import (
     StateDictType,
 )
 from torch.distributed.fsdp.wrap import transformer_auto_wrap_policy
+
+# Selective Checkpointing
 from train_fsdp import MP_POLICY_CONFIG, selective_ac
 
 # FP8 Transformer Engine
@@ -65,6 +67,8 @@ def train(
         cfg_path, bsz, n_workers, n_steps, grad_acc_steps, reduce_freq,
         sac_freq, pt_compile, compile_mode, profile, output_dir
     )
+    assert sac_freq == '1/1', 'Selective AC currently doesn\'t work with Transformer Engine.'
+    assert pt_compile == True, 'PyTorch compile currently doesn\'t work with Transformer Engine.'
 
     try:
         mp.spawn(train_fsdp_fp8, train_args, nprocs=world_size)
@@ -129,7 +133,7 @@ def train_fsdp_fp8(
 
         with torch.amp.autocast('cuda', torch.bfloat16):
             with te.fp8_autocast(enabled=True, fp8_recipe=fp8_recipe, fp8_group=all_gpus):
-                logits_BTV = model(input_BT, is_first_microbatch=False)
+                logits_BTV = model(input_BT, is_first_microbatch=(step_idx % grad_acc_steps == 0))
                 loss = F.cross_entropy(logits_BTV.flatten(0, 1), label_BT.flatten())
                 loss /= grad_acc_steps
 